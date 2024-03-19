c137cd5e55b4280da87e628059e5659fb038f79f
Christian Vergara <christianvd99@gmail.com>
2024-03-18 22:15:29 -0300

    update 2

--- src/views/HeroSection.jsx
+++ src/views/HeroSection.jsx
@@ -41,36 +41,6 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      {/* <div className="hero min-h-screen">
-        <div className="hero-content text-center text-neutral-content">
-          <div
-            className="max-w-md"
-            style={{
-              color: `hsl(${color.hue}, ${color.saturation * 100}%, ${
-                color.brightness * 100
-              }%)`,
-            }}
-          >
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button
-              className="p-5 text-black"
-              style={{
-                backgroundColor: `hsl(${color.hue}, ${
-                  color.saturation * 100
-                }%, ${color.brightness * 100}%)`,
-                borderRadius: `${rangeValue}px`,
-              }}
-            >
-              Get Started
-            </button>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
